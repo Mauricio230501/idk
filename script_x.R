61433fb6b4af32baba09775ab5a883c4bdebb6d2
Mau <mmeraz_23@alumnos.uaq.com>
2024-03-06 10:34:07 -0600

    cambio 2

--- script_x.R
+++ script_x.R
@@ -1,5 +1,10 @@
 función_1 <- function(a,b){
-  print(paste("la suma de dos", a+b))
+  print(paste("la suma de dos numeros", a+b))
 }
 función_1(1,2)
 función_1(2,5)
+función_1(7,9)
+
+
+git config --global user.email "mmeraz_23@alumnos.uaq.com"
+git config --global user.name "Mau"
